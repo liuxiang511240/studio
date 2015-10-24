2d2361676ed470dc05b8498c79e17de8aeee8d17
tony <liuxiang511240@163.com>
2015-10-24 12:13:43 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zxwang::Application.config.secret_key_base = '7c073e835fdd362e2e6e638894dba04183ba0a248203af0d47deb6e3eac41608c135318f33e7f57581ed7935b8525640cba6ca3d936e52e82306cde7fcd3ea50'
