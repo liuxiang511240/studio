7e1698a918ae6ca03767bfbdc31a31cce4e70a12
tony <liuxiangping@jiemian.com>
2015-11-10 19:47:31 +0800

    send

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$('.swiper-container').swiper({
+    slidesPerView: 2,
+    spaceBetween: 20,
+    prevButton: $('.wrap .icon-left'),
+    nextButton: $('.wrap .icon-right')
+});
